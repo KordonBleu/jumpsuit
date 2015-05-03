a3a714cc39d852218a9a2bb9eb25bb61e4e0b644
Getkey <getkey@gmx.fr>
2015-05-03 19:16:44 +0200

    Add background

--- canvas.js
+++ canvas.js
@@ -1,10 +1,23 @@
 "use strict";
 var canvas = document.getElementById('canvas')
+var context = canvas.getContext("2d");
 
-function sizeCanvas() {
+function setUpCanvas() {
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
+
+	context.fillStyle = "#0A0B12";
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	context.fillStyle = "white";
+	for(var i = 0; i != canvas.width; i++) {
+		for(var j = 0; j != canvas.height; j++) {
+			if(Math.random() < 0.0001) {
+				console.log(i, j);
+				context.fillRect(i, j, 1, 1);
+			}
+		}
+	}
 }
 
-sizeCanvas();
-window.addEventListener("resize", sizeCanvas);
+setUpCanvas();
+window.addEventListener("resize", setUpCanvas);
